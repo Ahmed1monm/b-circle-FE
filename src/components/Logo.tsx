2a59b2d6e6e6cf8228bb3a2e1c0f322fdf1a47c8
Salma Ahmed <salma_ahmed@icloud-ready.co>
2024-10-13 21:14:20 +0300

    add Navbar component and change theme

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import { Pencil } from "lucide-react";
+
+export default function Logo() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <Pencil />
+      <span className="font-semibold font-poppins text-nowrap">b-circle</span>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Menu, Search } from "lucide-react";
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "./mode-toggle";
+import { cn } from "@/lib/utils";
+
+export default function Navbar() {
+  const [state, setState] = React.useState(false);
+
+  const scrollTop = useScrollTop();
+  return (
+    <nav
+      className={cn(
+        "dark:bg-[#1f1f1f] fixed top-0 left-0 right-0 z-50 bg-background p-6 flex items-center",
+        scrollTop && "shadow-sm border-b"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-6 w-full">
+        <Link to="/" className="text-lg font-semibold">
+          Circles
+        </Link>
+        <Link to="/about" className="text-lg font-semibold">
+          Articles
+        </Link>
+       
+       
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
