0978a5db2dec3855e53dfa639de74fa238fb60d6
Salma Ahmed <salma_ahmed@icloud-ready.co>
2024-10-13 21:13:35 +0300

    Refactor project structure and update page title and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./theme-provider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="text-lg font-bold text-center">
-        Hello
-      </div>
-    </>
-  )
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
