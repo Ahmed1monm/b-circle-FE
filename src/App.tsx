4dd964bd821059eaf90f87a1cebac5e4b79fe7f0
Salma Ahmed <salma_ahmed@icloud-ready.co>
2024-10-18 15:32:31 +0300

    Refactor App component to add Toaster for displaying notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Circles from "./pages/circles";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container mx-auto mt-16 h-screen p-6">
+      <div className="container mx-auto mt-20 min-h-[calc(100vh-80px)] p-6">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/circles" element={<Circles />} />
         </Routes>
       </div>
+      <Toaster />
     </div>
   );
 }
